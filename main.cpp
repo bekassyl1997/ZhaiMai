862d0bb72446bc395253d571f0a0ddf10cd75610
bekassyl1997 <36645680+bekassyl1997@users.noreply.github.com>
2019-12-12 14:03:39 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Kazakhstan is the best country in the world!!!" << endl;
+	cout << "Nazarbayev University" << endl;
+	return 0;
+}
